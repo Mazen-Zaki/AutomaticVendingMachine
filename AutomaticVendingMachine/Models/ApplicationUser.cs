d826717b90419dc586082f23d5aa2afb54a364bf
hp <hp@VECO>
2022-12-17 17:12:15 +0200

    Add project files.

---
+++ AutomaticVendingMachine/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace AutomaticVendingMachine.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(1000)]
+        public string Address { get; set; }
+
+        public byte[] ProfilePicture { get; set; }
+
+    }
+}
